0e7196758773cde4c74d3f39afee04c5107453ee
MarkCDavid <40147713+MarkCDavid@users.noreply.github.com>
2019-03-18 14:21:29 +0200

    Added base controllers.

---
+++ src/controllers/LController.h
@@ -0,0 +1,15 @@
+//
+// Created by Mark David on 18/03/2019.
+//
+
+#ifndef TETRISVS_LCONTROLLER_H
+#define TETRISVS_LCONTROLLER_H
+
+#include "controller.h"
+
+class LController : public AbstractController {
+
+};
+
+
+#endif //TETRISVS_LCONTROLLER_H
